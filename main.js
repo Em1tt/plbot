1dbda0a2d04dfaa413173c53a6484373713dcd70
Em1tt <55552821+Em1tt@users.noreply.github.com>
2021-01-26 23:34:51 +0100

    Object destructuring
    Use more object destructuring you donut!

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const { Client } = require("discord.js");
 const bot = new Client();
-const config = require("./config.json");
+const {prefix} = require("./config.json");
 
 // im testing shit out so dont mind the bad code
 bot.once("ready", () => console.log("logged in"));
 bot.on("message", (msg) => {
-  if (!msg.content.startsWith(config.prefix))
+  if (!msg.content.startsWith(prefix))
     return;
 
-  let args = msg.content.slice(config.prefix.length).split(" ");
+  let args = msg.content.slice(prefix.length).split(" ");
   let cmd = args[0].toLowerCase();
   console.log(args);
   console.log(cmd);
